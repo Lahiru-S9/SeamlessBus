ac5702f4e8ff6337caf9412507669d86163477f7
dyonwalter <135725036+dyonwalter@users.noreply.github.com>
2023-10-30 14:06:49 +0530

    added footer to reg

--- app/views/gPassengers/register.php
+++ app/views/gPassengers/register.php
@@ -37,5 +37,32 @@
         </form>
     </div>
 </div>
+<div class="footer">
+        <img id="footer-logo" src="<?php echo URLROOT; ?>/img/logo_bw.png">
+        <div class="footer-subtext">
+            Seamless Bus
+        </div>
+        <div class="footer-text">
+            Enhancing your Travel Experience
+        </div>
+
+        <div class="social-media-icons">
+            <a href="#" class="social-media-icon">
+                <img src="<?php echo URLROOT; ?>/img/Facebook.png" alt="Facebook">
+            </a>
+            <a href="#" class="social-media-icon">
+                <img src="<?php echo URLROOT; ?>/img/Twitter.png" alt="Twitter">
+            </a>
+            <a href="#" class="social-media-icon">
+                <img src="<?php echo URLROOT; ?>/img/Instagram.png" alt="Instagram">
+            </a>
+        </div>
+    
+        <div class="footer-subtext">
+            Developed by CS group 23
+        </div>
+    </div>
+    
+</div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
