aea49e2cd6d16986e01d12a87dbb8c3d5106e9ff
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2023-10-30 23:36:56 +0530

    bf pull

--- app/controllers/Schedule.php
+++ app/controllers/Schedule.php
@@ -3,14 +3,16 @@
     class Schedule extends Controller{
         public function __construct(){
           $this->scheduleModel = $this->model('Schedulerow');
+          $this->stationModel = $this->model('Station');
         }
 
         public function index(){
             //Get Schedule
             $schedule = $this->scheduleModel->getSchedule();
-
+            $stations = $this->stationModel->getAllStations();
             $data =  [
                 'schedule' => $schedule,
+                'stations' => $stations,
             ];
    
             $this->view('schedule/index', $data);
