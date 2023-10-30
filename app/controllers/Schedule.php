a4f4db7ddd592a49a2c707862575d61ebf208f49
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2023-10-31 00:15:06 +0530

    booking schedule

--- app/controllers/Schedule.php
+++ app/controllers/Schedule.php
@@ -7,15 +7,47 @@
         }
 
         public function index(){
+            $stations = $this->stationModel->getAllStations();
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $data = [
+                    'from' => trim($_POST['from']),
+                    'to' => trim($_POST['to']),
+                    'from_err' => '',
+                    'to_err' => '',
+                ];
+
+
+                if(empty($data['from'])){
+                    $data['from_err'] = 'Please select a station';
+                }
+
+                if(empty($data['to'])){
+                    $data['to_err'] = 'Please select a station';
+                }
+
+                if(empty($data['from_err']) && empty($data['to_err'])){
+                    $schedule = $this->scheduleModel->getScheduleByStation($data['from'], $data['to']);
+                    $data =  [
+                        'schedule' => $schedule,
+                        'stations' => $stations,
+                    ];
+                    $this->view('schedule/index', $data);
+                }else{
+                    $this->view('schedule/index', $data);
+                }
+            }else{
             //Get Schedule
             $schedule = $this->scheduleModel->getSchedule();
-            $stations = $this->stationModel->getAllStations();
+            
             $data =  [
                 'schedule' => $schedule,
                 'stations' => $stations,
             ];
    
             $this->view('schedule/index', $data);
+            }
         }
 
         
--- app/models/Schedulerow.php
+++ app/models/Schedulerow.php
@@ -22,4 +22,17 @@ class Schedulerow {
         return $results;
     }
 
+    public function getScheduleByStation($from, $to) {
+        $this->db->query('SELECT *,
+                            stations.station AS from_station, 
+                            s.station AS to_station
+                            FROM schedule AS sc
+                            JOIN routes AS r ON sc.routeid = r.id
+                            JOIN stations AS stations ON r.fromstationid = stations.id
+                            JOIN stations AS s ON r.tostationid = s.id
+                            WHERE stations.id = :from AND s.id = :to
+                         ');
+    }
+    
+
 }
