6912cb863d1b49e98ee30fa6d6ad0c2f601e1097
tharikathushani <tharikathushani212@gmail.com>
2023-10-31 20:30:43 +0530

    modify scheduler

---
+++ app/views/schedulers/index1.php
@@ -0,0 +1,7 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<link rel="stylesheet" href = "<?php echo URLROOT; ?>/css/schedulers/style1.css">
+
+<div class="item1">loging</div>
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
