0d5ec02c2450d7751bf0c4a542cdf56cd7b30426
tharikathushani <tharikathushani212@gmail.com>
2023-10-31 11:46:12 +0530

    #dashbord schedular

---
+++ app/views/schedulers/dashboard.php
@@ -0,0 +1,20 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<link rel="stylesheet" href = "<?php echo URLROOT; ?>/css/schedulers/dashboard.css">
+
+
+
+<!-- <div class="picture" src="/public/img/cartoon_conductor.png" ></div> -->
+<div class="item1"> <h3>schedule the seamless bus</h3></div>
+<br>
+<div class="item3">
+    <div class="item2">
+        <button class="item" type="button">assest1</button>
+        <button class="item" type="button">assest2</button>
+        <button class="item" type="button">assest3</button>
+        <button class="item" type="button">assest4</button>
+       
+    </div>
+    </div>
+    <div class="picture" src="/img/cartoon_conductor.png" ></div>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
