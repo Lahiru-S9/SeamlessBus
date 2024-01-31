d5f44f7f531a7397f7c745259e1ad1ef05a4180a
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2024-01-31 20:39:20 +0530

    Refactor Scheduler model and update CSS and JS files

--- app/models/Scheduler.php
+++ app/models/Scheduler.php
@@ -19,12 +19,12 @@ class Scheduler {
         // GROUP BY schedulers.scheduler_id;
         // ');
 
-        $this->db->query('SELECT users.name, schedulers.scheduler_id,
+        $this->db->query('SELECT users.name, scheduler_details.id AS scheduler_id,
         GROUP_CONCAT(schedulers.station_id) AS station_ids,
         GROUP_CONCAT(stations.station) AS station_names
         FROM users
         JOIN scheduler_details ON users.id = scheduler_details.user_id
-        JOIN schedulers ON schedulers.scheduler_id = scheduler_details.id
+        LEFT JOIN schedulers ON schedulers.scheduler_id = scheduler_details.id
         LEFT JOIN stations ON schedulers.station_id = stations.id
         GROUP BY schedulers.scheduler_id;');
         
