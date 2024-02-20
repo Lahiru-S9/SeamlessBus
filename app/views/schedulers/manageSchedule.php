65761f6ac704843a86a129c9bacece5538f7c25e
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2024-02-20 08:26:11 +0530

    Add manageSchedule function and update dashboard action label

---
+++ app/views/schedulers/manageSchedule.php
@@ -0,0 +1,323 @@
+<!-- Include the header file -->
+<?php include_once APPROOT . '/views/inc/header.php'; ?>
+<link rel="stylesheet" href = "<?php echo URLROOT; ?>/css/schedulers/manageSchedule.css">
+
+<div class="tabs">
+  <div class="tab">
+    <input type="radio" id="tab-1" name="tab-group-1" checked>
+    <label for="tab-1">Monday</label>
+    
+    <div class="content">
+      <!-- Your table goes here for Tab One -->
+      <div class="container">
+        <div class="table">
+            <div class="table-header">
+                <div class="header__item"><a id="route_num" class="filter__link filter__link--number" href="#">Route Num</a></div>
+                <div class="header__item"><a id="arrival" class="filter__link filter__link--number" href="#">Arrival Time</a></div>
+                <div class="header__item"><a id="departure" class="filter__link filter__link--number" href="#">Departure Time</a></div>
+                <div class="header__item"><a id="from" class="filter__link" href="#">From station</a></div>
+                <div class="header__item"><a id="to" class="filter__link" href="#">To station</a></div>
+            </div>
+            <div class="table-content">	
+                <div class="table-row">		
+                    <div class="table-data">Tom</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">5</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Dick</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">3</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Harry</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                </div>
+            </div>	
+        </div>
+     </div>
+    </div>
+ </div> 
+
+  
+  <div class="tab">
+    <input type="radio" id="tab-2" name="tab-group-1">
+    <label for="tab-2">Tuesday</label>
+    
+    <div class="content">
+      <!-- Your table goes here for Tab Two -->
+      <div class="container">
+        <div class="table">
+        <div class="table-header">
+                <div class="header__item"><a id="route_num" class="filter__link filter__link--number" href="#">Route Num</a></div>
+                <div class="header__item"><a id="arrival" class="filter__link filter__link--number" href="#">Arrival Time</a></div>
+                <div class="header__item"><a id="departure" class="filter__link filter__link--number" href="#">Departure Time</a></div>
+                <div class="header__item"><a id="from" class="filter__link" href="#">From station</a></div>
+                <div class="header__item"><a id="to" class="filter__link" href="#">To station</a></div>
+            </div>
+            <div class="table-content">	
+                <div class="table-row">		
+                    <div class="table-data">Tom</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">5</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Dick</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">3</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Harry</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                </div>
+            </div>	
+        </div>
+     </div>
+    </div>
+ </div> 
+
+  
+  <div class="tab">
+    <input type="radio" id="tab-3" name="tab-group-1">
+    <label for="tab-3">Wednesday</label>
+    
+    <div class="content">
+      <!-- Your table goes here for Tab Three -->
+      <div class="container">
+        <div class="table">
+            <div class="table-header">
+                <div class="header__item"><a id="route_num" class="filter__link filter__link--number" href="#">Route Num</a></div>
+                <div class="header__item"><a id="arrival" class="filter__link filter__link--number" href="#">Arrival Time</a></div>
+                <div class="header__item"><a id="departure" class="filter__link filter__link--number" href="#">Departure Time</a></div>
+                <div class="header__item"><a id="from" class="filter__link" href="#">From station</a></div>
+                <div class="header__item"><a id="to" class="filter__link" href="#">To station</a></div>
+            </div>
+            <div class="table-content">	
+                <div class="table-row">		
+                    <div class="table-data">Tom</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">5</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Dick</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">3</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Harry</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                </div>
+            </div>	
+        </div>
+
+     </div> 
+    </div>
+ </div>
+
+ <div class="tab">
+    <input type="radio" id="tab-4" name="tab-group-1">
+    <label for="tab-4">Thursday</label>
+    
+    <div class="content">
+      <!-- Your table goes here for Tab Three -->
+      <div class="container">
+        <div class="table">
+            <div class="table-header">
+                <div class="header__item"><a id="route_num" class="filter__link filter__link--number" href="#">Route Num</a></div>
+                <div class="header__item"><a id="arrival" class="filter__link filter__link--number" href="#">Arrival Time</a></div>
+                <div class="header__item"><a id="departure" class="filter__link filter__link--number" href="#">Departure Time</a></div>
+                <div class="header__item"><a id="from" class="filter__link" href="#">From station</a></div>
+                <div class="header__item"><a id="to" class="filter__link" href="#">To station</a></div>
+            </div>
+            <div class="table-content">	
+                <div class="table-row">		
+                    <div class="table-data">Tom</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">5</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Dick</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">3</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Harry</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                </div>
+            </div>	
+        </div>
+
+     </div> 
+    </div>
+ </div>
+
+ <div class="tab">
+    <input type="radio" id="tab-5" name="tab-group-1">
+    <label for="tab-5">Friday</label>
+    
+    <div class="content">
+      <!-- Your table goes here for Tab Three -->
+      <div class="container">
+        <div class="table">
+            <div class="table-header">
+                <div class="header__item"><a id="route_num" class="filter__link filter__link--number" href="#">Route Num</a></div>
+                <div class="header__item"><a id="arrival" class="filter__link filter__link--number" href="#">Arrival Time</a></div>
+                <div class="header__item"><a id="departure" class="filter__link filter__link--number" href="#">Departure Time</a></div>
+                <div class="header__item"><a id="from" class="filter__link" href="#">From station</a></div>
+                <div class="header__item"><a id="to" class="filter__link" href="#">To station</a></div>
+            </div>
+            <div class="table-content">	
+                <div class="table-row">		
+                    <div class="table-data">Tom</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">5</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Dick</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">3</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Harry</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                </div>
+            </div>	
+        </div>
+
+     </div> 
+    </div>
+ </div>
+
+ <div class="tab">
+    <input type="radio" id="tab-6" name="tab-group-1">
+    <label for="tab-6">Saturday</label>
+    
+    <div class="content">
+      <!-- Your table goes here for Tab Three -->
+      <div class="container">
+        <div class="table">
+            <div class="table-header">
+                <div class="header__item"><a id="route_num" class="filter__link filter__link--number" href="#">Route Num</a></div>
+                <div class="header__item"><a id="arrival" class="filter__link filter__link--number" href="#">Arrival Time</a></div>
+                <div class="header__item"><a id="departure" class="filter__link filter__link--number" href="#">Departure Time</a></div>
+                <div class="header__item"><a id="from" class="filter__link" href="#">From station</a></div>
+                <div class="header__item"><a id="to" class="filter__link" href="#">To station</a></div>
+            </div>
+            <div class="table-content">	
+                <div class="table-row">		
+                    <div class="table-data">Tom</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">5</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Dick</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">3</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Harry</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                </div>
+            </div>	
+        </div>
+
+     </div> 
+    </div>
+ </div>
+
+ <div class="tab">
+    <input type="radio" id="tab-7" name="tab-group-1">
+    <label for="tab-7">Sunday</label>
+    
+    <div class="content">
+      <!-- Your table goes here for Tab Three -->
+      <div class="container">
+        <div class="table">
+            <div class="table-header">
+                <div class="header__item"><a id="route_num" class="filter__link filter__link--number" href="#">Route Num</a></div>
+                <div class="header__item"><a id="arrival" class="filter__link filter__link--number" href="#">Arrival Time</a></div>
+                <div class="header__item"><a id="departure" class="filter__link filter__link--number" href="#">Departure Time</a></div>
+                <div class="header__item"><a id="from" class="filter__link" href="#">From station</a></div>
+                <div class="header__item"><a id="to" class="filter__link" href="#">To station</a></div>
+            </div>
+            <div class="table-content">	
+                <div class="table-row">		
+                    <div class="table-data">Tom</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">5</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Dick</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">1</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">3</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-data">Harry</div>
+                    <div class="table-data">0</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                    <div class="table-data">2</div>
+                </div>
+            </div>	
+        </div>
+
+     </div> 
+    </div>
+ </div>
+
+ 
+</div>
+
+
+
+
+
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
