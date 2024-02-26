479a9bc721fa56bf62527c2fe14ade6a184d320d
tharikathushani <tharikathushani212@gmail.com>
2024-02-26 09:15:31 +0530

    scheduler changers

--- app/views/schedulers/conductors.php
+++ app/views/schedulers/conductors.php
@@ -1,7 +1,28 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- background image -->
+<div class="container">
+<img src="/public/img/4.png" style="width: 65%;">
+  <div>
+   <!-- create button1 -->
+   <button class="button1" name="logingButton1" 
+      onclick="location.href='verifyConductor.php'">
+      <i class="fa-solid fa-user-plus"></i>register
+    
+    </button>
+    <!-- end button -->
+<br>
+          <!-- create button1 -->
+   <button class="button1" name="logingButton1" 
+      onclick="location.href='SeeConductorDetails.php'">
+      <i class="fa-solid fa-user-plus"></i>register
+    
+    </button>
+    <!-- end button -->
+  </div>
+</div>
+<!-- end background image -->
 <!-- import dashbord icons -->
 <?php require APPROOT . '/views/schedulers/importDashboard.php' ; ?>
 <!-- end dashbord icons -->
--- app/views/schedulers/dashboard.php
+++ app/views/schedulers/dashboard.php
@@ -1,66 +1,16 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<link rel="stylesheet" href = "<?php echo URLROOT; ?>/css/schedulers/dashboard.css">
+<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/schedulers/dashbord.css">
+
+
 <div class="container">
     <div class="dashboard-content">
         <h1 class="dashboard-title">What's on your mind?</h1>
         <p class="dashboard-description">Enhancing your Travel Experience</p>
-        
     </div>
-    <div class="dashboard-actions">
-<<<<<<< HEAD
+</div>
         
-        
-        
-        <div class = "popup" id = "popup-1">
-            <div class = "overlay"></div>
-            <div class = "content">
-                <div class = "close-btn" onclick = "togglePopup()">&times;</div>
-                <h1>My QR</h1>
-                <input id="text" type="hidden" value= "<?php echo $data['encrypted_data'];?>" style="width:80%" /><br />
-                <div id="qrcode"></div>
-            </div>
-        </div>
-        <!-- <button class="popup-button" onclick="togglePopup()">Show QR</button> -->
-        <a  class="dashboard-action" onclick = "togglePopup()">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/QR.png" alt="QR">
-            </div>
-            <p class="action-label">Conductors</p>
-        </a>
-        
-        <!-- Add more dashboard actions as needed -->
-        <a href="<?php echo URLROOT;?>/Schedulers/busDetails" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/Routines.png" alt="QR">
-            </div>
-            <p class="action-label">Busses</p>
-        </a>
-
-        <!-- Add more dashboard actions as needed -->
-        <a href="#" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/payment.png" alt="QR">
-            </div>
-            <p class="action-label">Schedule</p>
-        </a>
-
-        <!-- Add more dashboard actions as needed -->
-        <a href="<?php echo URLROOT?>/schedulers/profile" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/Group.png" alt="QR">
-            </div>
-            <p class="action-label">booking lists</p>
-        </a>
-
-          <!-- Add more dashboard actions as needed -->
-          <a href="<?php echo URLROOT?>/schedulers/profile" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/Group.png" alt="QR">
-            </div>
-            <p class="action-label">feedbacks</p>
-        </a>
-    </div>
+<?php require APPROOT . '/views/schedulers/importDashboard.php' ; ?>
 
 
 <div class="footer">
@@ -89,69 +39,7 @@
         </div>
     </div>
     
-</div>
-
-=======
-        <a href="#" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/QR.png" alt="QR">
-            </div>
-            <p class="action-label">Edit schedule</p>
-        </a>
-
-        <!-- Add more dashboard actions as needed -->
-        <a href="<?php echo URLROOT;?>/Schedulers/busDetails" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/Routines.png" alt="QR">
-            </div>
-            <p class="action-label">Bus details</p>
-        </a>
-
-        <!-- Add more dashboard actions as needed -->
-        <a href="#" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/payment.png" alt="QR">
-            </div>
-            <p class="action-label">Bookings</p>
-        </a>
-
-        <!-- Add more dashboard actions as needed -->
-        <a href="<?php echo URLROOT?>/regPassengers/profile" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/Group.png" alt="QR">
-            </div>
-            <p class="action-label">Profile details</p>
-        </a>
-    </div>
-
-    <div class="footer">
-            <img src="<?php echo URLROOT; ?>/img/logo_bw.png">
-            <div class="footer-subtext">
-                Seamless Bus
-            </div>
-            <div class="footer-text">
-                Enhancing your Travel Experience
-            </div>
-
-            <div class="social-media-icons">
-                <a href="#" class="social-media-icon">
-                    <img src="<?php echo URLROOT; ?>/img/Facebook.png" alt="Facebook">
-                </a>
-                <a href="#" class="social-media-icon">
-                    <img src="<?php echo URLROOT; ?>/img/Twitter.png" alt="Twitter">
-                </a>
-                <a href="#" class="social-media-icon">
-                    <img src="<?php echo URLROOT; ?>/img/Instagram.png" alt="Instagram">
-                </a>
-            </div>
-            
-            <div class="footer-subtext">
-                Developed by CS group 23
-            </div>
-    </div>
-
-</div>
 
 
->>>>>>> c08a86354e448b1d227e4dd5ffd60e2e87968653
+
 <?php require APPROOT . '/views/inc/footer.php'; ?>
