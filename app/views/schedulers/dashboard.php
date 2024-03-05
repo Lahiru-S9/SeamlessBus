972b9c915f37f5a697344cfb4abb1b12105896ea
tharikathushani <tharikathushani212@gmail.com>
2024-03-05 21:43:46 +0530

    changes

--- app/views/schedulers/dashboard.php
+++ app/views/schedulers/dashboard.php
@@ -8,70 +8,7 @@
         <h1 class="dashboard-title">What's on your mind?</h1>
         <p class="dashboard-description">Enhancing your Travel Experience</p>
     </div>
-<<<<<<< HEAD
-=======
-    <div class="dashboard-actions">
-        <a href="<?php echo URLROOT?>/Schedulers/manageSchedule" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/QR.png" alt="QR">
-            </div>
-            <p class="action-label">Manage Schedule</p>
-        </a>
 
-        <!-- Add more dashboard actions as needed -->
-        <a href="<?php echo URLROOT;?>/Schedulers/busDetails" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/Routines.png" alt="QR">
-            </div>
-            <p class="action-label">Bus details</p>
-        </a>
-
-        <!-- Add more dashboard actions as needed -->
-        <a href="#" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/payment.png" alt="QR">
-            </div>
-            <p class="action-label">Bookings</p>
-        </a>
-
-        <!-- Add more dashboard actions as needed -->
-        <a href="<?php echo URLROOT?>/regPassengers/profile" class="dashboard-action">
-            <div class="action-icon" style="background: #62D9CC;">
-                <img src="<?php echo URLROOT; ?>/img/Group.png" alt="QR">
-            </div>
-            <p class="action-label">Profile details</p>
-        </a>
-    </div>
-
-    <div class="footer">
-            <img src="<?php echo URLROOT; ?>/img/logo_bw.png">
-            <div class="footer-subtext">
-                Seamless Bus
-            </div>
-            <div class="footer-text">
-                Enhancing your Travel Experience
-            </div>
-
-            <div class="social-media-icons">
-                <a href="#" class="social-media-icon">
-                    <img src="<?php echo URLROOT; ?>/img/Facebook.png" alt="Facebook">
-                </a>
-                <a href="#" class="social-media-icon">
-                    <img src="<?php echo URLROOT; ?>/img/Twitter.png" alt="Twitter">
-                </a>
-                <a href="#" class="social-media-icon">
-                    <img src="<?php echo URLROOT; ?>/img/Instagram.png" alt="Instagram">
-                </a>
-            </div>
-            
-            <div class="footer-subtext">
-                Developed by CS group 23
-            </div>
-    </div>
-
->>>>>>> a7a16673e074021c5a012bec17e019ab950f2a43
-</div>
-        
 <?php require APPROOT . '/views/schedulers/importDashboard.php' ; ?>
 
 
