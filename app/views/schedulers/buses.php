31b1891b4db6b4f9dafba0cb1e257001709be949
tharikathushani <tharikathushani212@gmail.com>
2024-03-08 19:27:22 +0530

    create schedulers dashboards

--- app/views/schedulers/buses.php
+++ app/views/schedulers/buses.php
@@ -1,4 +1,45 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
+<link rel="stylesheet" href = "<?php echo URLROOT; ?>/css/schedulers/buses.css">
+<!-- background image -->
+
+<div class="container" >
+<div class="image-container">
+    <img src="<?php echo URLROOT; ?>/img/1.png" alt="Background Image" class="background-image">
+  <div class="left">
+      <div class="text-overlay">
+                <h1 class="overlay-text">You Have Full Rights To Handle <br><br> buses!!</h1>
+                <h4 class="text2"> Click On Your Need?<h4>
+      <div class="buttons-container">
+<!--1 button image -->
+        <div class="clickheres">
+        <a href="/app/views/schedulers/verifyBus.php"> 
+        <img src="<?php echo URLROOT;?> /public/img/verify bus.png" class="image"></a>
+        </div>
+<!-- 1 end button image -->
+<!-- 2 end button image -->
+        <div class="clickheres">
+        <a href="/app/views/schedulers/busdetails.php"> 
+        <img src="<?php echo URLROOT;?>/public/img/seeBusDetails.png"  class="image"></a>
+        </div>
+        
+ <!-- 2 nd button end-->
+
+ <!-- 3 end button image -->
+ <div class="clickheres">
+        <a href="/app/views/schedulers/AddBusRotation.php"> 
+        <img src="<?php echo URLROOT;?>/public/img/addbus rotation.png"  class="image"></a>
+        </div>
+        
+ <!-- 3 nd button end-->
+
+ </div><!--for button-->
+</div> <!--for text-->
+</div>
+</div> <!--image container-->
+</div>
+<!-- end background image -->
+
+
 
 
 
