7e5ecf771f47eca1a40bbb35f44c5d95a064f776
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2024-03-11 00:41:53 +0530

    Add stylesheet for importDashboard.php

--- app/views/schedulers/importDashboard.php
+++ app/views/schedulers/importDashboard.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/schedulers/dashboard.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/schedulers/importDashboard.css">
     <title>Document</title>
 </head>
@@ -16,7 +17,6 @@
             <p class="action-label">Conductors</p>
         </a>
 
-<<<<<<< HEAD
         <!-- go to busses dashboard -->
         <a href="<?php echo URLROOT ?>/Schedulers/buses" class="dashboard-action">
             <div class="action-icon" style="background: #62D9CC;">
@@ -27,10 +27,6 @@
 
         <!-- go to schedulers dashboard -->
         <a id="schedule-dashboard" class="dashboard-action">
-=======
-   <!-- go to schedulers dashboard -->
-        <a href="<?php echo URLROOT ?>/Schedulers/manageSchedule" class="dashboard-action">
->>>>>>> f787b064a06841113bed05b5e8a4a8fb3299045d
             <div class="action-icon" style="background: #62D9CC;">
                 <img src="<?php echo URLROOT; ?>/img/owner.png" alt="QR">
             </div>
@@ -54,32 +50,6 @@
         </a>
     </div>
 
-    <!-- script file for onclick event handle -->
-    <script>
-        function handleDashboardAction(url) {
-            window.location.href = url;
-        }
-
-        // Add event listeners for each dashboard action
-        document.getElementById('conductor-dashboard').addEventListener('click', function() {
-            handleDashboardAction('<?php echo URLROOT ?>/schedulers/conductors.php');
-        });
-
-        document.getElementById('busses-dashboard').addEventListener('click', function() {
-            handleDashboardAction('<?php echo URLROOT ?>/schedulers/buses.php');
-        });
-
-        document.getElementById('schedule-dashboard').addEventListener('click', function() {
-            handleDashboardAction('<?php echo URLROOT ?>/schedulers/shedule.php');
-        });
-
-        document.getElementById('booking-lists-dashboard').addEventListener('click', function() {
-            handleDashboardAction('<?php echo URLROOT?>/schedulers/booking1.php');
-        });
-
-        document.getElementById('feedbacks-dashboard').addEventListener('click', function() {
-            handleDashboardAction('<?php echo URLROOT ?>/schedulers/feedbackForm.php');
-        });
-    </script>
+   
 </body>
 </html>
