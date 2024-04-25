080595187564623f36040db210ceb9d27638504e
Tharushi Hansima <tharushihansima23@gmail.com>
2024-04-24 22:02:04 -0700

    backend for revenue

--- app/views/Owners/SeeReport.php
+++ app/views/Owners/SeeReport.php
@@ -30,19 +30,27 @@
         <table>
             <thead>
                 <tr>
-                    <th>Date</th>
-                    <th>Bus</th>
-                    <th>Revenue</th>
+                    <th>Bus Number</th>
+                    <th>Booking Count</th>
+                    <th>Ticket Price</th>
+                    <th>Total Revenue</th>
                     <!-- Add more column headers -->
                 </tr>
             </thead>
             <tbody>
                 <!-- Populate table rows with sales data -->
+                <?php foreach ($data['revenue_data'] as $revenue):?>
                 <tr> 
+                        <td><?php echo $revenue->bus_no; ?></td>
+                        <td><?php echo $revenue->booking_count; ?></td>
+                        <td><?php echo $revenue->ticket_price; ?></td>
+                        <td><?php echo $revenue->total_revenue; ?></td>
+
+
                     <!-- dummy data for reports -->
-                    <td>2024-04-01</td>
+                    <!--<td>2024-04-01</td>
                     <td>Rosa</td>
-                    <td>$XXX</td>
+                    <td>$XXX</td> -->
                     <!-- Add more columns -->
                 </tr>
                 <!-- Add more rows -->
