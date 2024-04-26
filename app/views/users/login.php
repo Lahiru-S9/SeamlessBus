108606cee9ff0f81aa5b4800e8d7a1bb0cab4eb1
dyonwalter <135725036+dyonwalter@users.noreply.github.com>
2024-04-26 20:44:15 +0530

    login-page-modification and footer-modification

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,28 +1,26 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/users/login.css">
 
-<div class="container">
-<div class="login-subtitle">Enter login credentials</div>
-<div class="login-title">Log into your profile</div>
-    <div class="login-card">
-        <?php flash('register_success'); ?>
-        
-        
-        <form action="<?php echo URLROOT; ?>/users/login" method="POST">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="text" id="email" name="email" class="custom-input <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+<div class="login-box">
+   <center><h2>Enter login credentials</h2></center>
+<?php flash('register_success'); ?>
+  <form id="login-form" action="<?php echo URLROOT; ?>/users/login" method="POST">
+    <div class="user-box">
+    <input required="" type="text" id="email" name="email" class="custom-input <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
                 <span class="error-message"><?php echo $data['email_err']; ?></span>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" class="custom-input <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
-                <span class="error-message"><?php echo $data['password_err']; ?></span>
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn">Login</button>
-            </div>
-        </form>
+      <label for="email">Email Address</label>
     </div>
+    <div class="user-box">
+    <input required="" type="password" id="password" name="password" class="custom-input <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
+                <span class="error-message"><?php echo $data['password_err']; ?></span>
+      <label for="password">Password</label>
+    </div><center>
+    <a onclick="document.getElementById('login-form').submit();">
+           LOGIN
+       <span></span>
+    </a></center>
+  </form>
 </div>
+<div class="login-footer">
 <?php require APPROOT . '/views/inc/footer.php'; ?>
+</div>
