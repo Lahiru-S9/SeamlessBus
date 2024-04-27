a9aba99f8ec98a25905a986c239c679a9bbce42a
dyonwalter <135725036+dyonwalter@users.noreply.github.com>
2024-04-27 16:59:39 +0530

    dashboard-modified and fixed

--- app/views/schedulers/dashboard.php
+++ app/views/schedulers/dashboard.php
@@ -1,44 +1,12 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-
-<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/schedulers/dashbord.css">
-
+<link rel="stylesheet" href = "<?php echo URLROOT; ?>/css/regPassengers/dashboard.css">
 
 <div class="container">
-    <div class="dashboard-content">
-        <h1 class="dashboard-title">What's on your mind?</h1>
-        <p class="dashboard-description">Enhancing your Travel Experience</p>
+    <div class="title">What's on your mind?</div>
+    <div class="card-container">
+        <div id="bus-card" class="card" onclick="window.location.href='<?php echo URLROOT ?>/Schedulers/buses'">Busses</div>
+        <div id="schedule-card" class="card" onclick="window.location.href='<?php echo URLROOT ?>/Schedulers/manageSchedule'">Schedules</div>
+        <div id="feedback-card" class="card" onclick="window.location.href='<?php echo URLROOT ?>/Schedulers/feedbackForm'">Feedback</div>
     </div>
-
-<?php require APPROOT . '/views/schedulers/importDashboard.php' ; ?>
-
-
-<div class="footer">
-        <img id="footer-logo" src="<?php echo URLROOT; ?>/img/logo_bw.png">
-        <div class="footer-subtext">
-            Seamless Bus
-        </div>
-        <div class="footer-text">
-            Enhancing your Travel Experience
-        </div>
-
-        <div class="social-media-icons">
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Facebook.png" alt="Facebook">
-            </a>
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Twitter.png" alt="Twitter">
-            </a>
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Instagram.png" alt="Instagram">
-            </a>
-        </div>
-    
-        <div class="footer-subtext">
-            Developed by CS group 23
-        </div>
-    </div>
-    
-
-
-
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
