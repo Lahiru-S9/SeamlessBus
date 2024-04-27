e6d8b71df116b9bdaf9bf3f40cc25ef18d357639
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2024-04-28 01:54:24 +0530

    Update Schedulers.php, Route.php, and Station.php with new methods and modifications

--- app/models/Route.php
+++ app/models/Route.php
@@ -22,5 +22,39 @@ class Route {
         return $results;
     }
 
+    public function getRoutesbySchedulerWithDetails($id){
+        $this->db->query('SELECT 
+        routes.route_num,
+        tostation.station AS tostation,
+        fromstation.station AS fromstation,
+        COUNT(buses.bus_no) AS bus_count
+    FROM 
+        routes 
+    JOIN 
+        schedulers ON schedulers.station_id = routes.tostationid
+    JOIN 
+        stations AS tostation ON routes.tostationid = tostation.id
+    JOIN 
+        stations AS fromstation ON routes.fromstationid = fromstation.id
+    JOIN 
+        scheduler_details ON schedulers.scheduler_id = scheduler_details.id 
+    JOIN 
+        buses ON routes.route_num = buses.route_num
+    WHERE 
+        scheduler_details.user_id = :id
+    GROUP BY 
+        routes.route_num, tostation, fromstation;
+    ');
+        $this->db->bind(':id', $id);
+        $results = $this->db->resultSet();
+        return $results;
+    }
+
+    public function getRouteByRouteNumber($routeNumber){
+        $this->db->query('SELECT * FROM routes WHERE route_num = :route_num');
+        $this->db->bind(':route_num', $routeNumber);
+        $result = $this->db->single();
+        return $result;
+    }
     
 }
