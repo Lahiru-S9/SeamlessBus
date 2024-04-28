f3ba9d5cd2c15025497e8d2c5089b744b67bf76d
KL Sandaruwan <lahirusandaruwan501@gmail.com>
2024-04-28 12:26:04 +0530

    Refactor PerformanceMatrix.php view for improved revenue data display

--- app/views/Owners/PerformanceMatrix.php
+++ app/views/Owners/PerformanceMatrix.php
@@ -4,48 +4,16 @@
 <div class = "container">
     <h1>Performance Matrix</h1>
 
-    <div class="summary">
-        <div class="metric">
-            <h3>Total Revenue</h3>
-            <?php foreach($data['revenue_data'] as $revenue): ?>
-            <p class="<?php echo ($revenue->total_revenue >= 10000) ? 'good' : (($revenue->total_revenue >= 5000) ? 'average' : 'poor'); ?>">
-                <?php echo $revenue->total_revenue; ?>
-
-            </p>
-            <?php endforeach ; ?>
-        
+    <div>
+        <canvas id="myChart"></canvas>
     </div>
- 
-    
-    <!-- Add more performance metrics -->
+
+ <!-- Add more performance metrics -->
 </div>
 
     
-</div>
-<div class="footer">
-        <img id="footer-logo" src="<?php echo URLROOT; ?>/img/logo_bw.png">
-        <div class="footer-subtext">
-            Seamless Bus
-        </div>
-        <div class="footer-text">
-            Enhancing your Travel Experience
-        </div>
-
-        <div class="social-media-icons">
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Facebook.png" alt="Facebook">
-            </a>
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Twitter.png" alt="Twitter">
-            </a>
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Instagram.png" alt="Instagram">
-            </a>
-        </div>
-    
-        <div class="footer-subtext">
-            Developed by CS group 23
-        </div>
-    </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="<?php echo URLROOT; ?>/js/Owners/performanceMatrix.js"></script>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
 
 
