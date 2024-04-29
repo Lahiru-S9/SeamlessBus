d6227414cb8064b8df891dc490dd8b03814aaff1
tharikathushani <tharikathushani212@gmail.com>
2024-04-29 11:38:49 +0530

    remove dublicate footer

--- app/views/schedulers/SeeBusDetails.php
+++ app/views/schedulers/SeeBusDetails.php
@@ -62,31 +62,7 @@
 
 
 <!-- footer -->
-    <div class="footer">
-        <img id="footer-logo" src="<?php echo URLROOT; ?>/img/logo_bw.png">
-        <div class="footer-subtext">
-            Seamless Bus
-        </div>
-        <div class="footer-text">
-            Enhancing your Travel Experience
-        </div>
 
-        <div class="social-media-icons">
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Facebook.png" alt="Facebook">
-            </a>
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Twitter.png" alt="Twitter">
-            </a>
-            <a href="#" class="social-media-icon">
-                <img src="<?php echo URLROOT; ?>/img/Instagram.png" alt="Instagram">
-            </a>
-        </div>
-    
-        <div class="footer-subtext">
-            Developed by CS group 23
-        </div>
-    </div>
     
 </div>
 
